0e30f1204103dc2b04f1c1d4ae5abcea714435d0
kjrzxu <119322320+kjrzxu@users.noreply.github.com>
2023-01-17 00:04:39 +0800

    更新 settings.lua

--- luci-app-cloudflared/luasrc/model/cbi/cloudflared/settings.lua
+++ luci-app-cloudflared/luasrc/model/cbi/cloudflared/settings.lua
@@ -1,25 +1,25 @@
 
-a=Map("cloudflared",translate("Cloudflared"),translate("隧道客户端-以前的Argo Tunnel"))
+a=Map("cloudflared",translate("Cloudflared"),translate("Tunnel client - formerly Argo Tunnel"))
 a:section(SimpleSection).template  = "cloudflared/cloudflared_status"
 
 t=a:section(NamedSection,"sample_config","cloudflared")
 t.anonymous=true
 t.addremove=false
 
-e=t:option(Flag,"enabled",translate("开启"))
+e=t:option(Flag,"enabled",translate("Enable"))
 e.default=0
 e.rmempty=false
 
-e=t:option(DynamicList,"token",translate('隧道Token'))
+e=t:option(DynamicList,"token",translate('Tunnel Token'))
 e.password=true
 e.rmempty=false
 
-e=t:option(Flag,"free",translate("隧道使用免费随机的URL域名"))
+e=t:option(Flag,"free",translate("Tunnel with free random URL"))
 e.default=0
 e.rmempty=false
 
 e=t:option(DummyValue,"opennewwindow" , 
 	translate("<input type=\"button\" class=\"cbi-button cbi-button-apply\" value=\"cloudflared.com\" onclick=\"window.open('https://dash.teams.cloudflare.com')\" />"))
-e.description = translate("创建或管理你的cloudflare隧道")
+e.description = translate("Create or manage your cloudflared tunnel")
 
 return a
